2c62ea31f870cfc6dc3a2ad274a73c1b7ff23cc1
2044smile <2044smile@naver.com>
2022-11-04 11:02:01 +0900

    refactor: add comment

--- tstation.py
+++ tstation.py
@@ -1,4 +1,4 @@
-import os, json, time, requests
+import json, time, requests
 
 from bs4 import BeautifulSoup
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -9,7 +9,7 @@ class tstation:
         self.headers = {"Content-Type": "application/json"}
         self.tstation = requests.get(url='https://news.google.com/search?q=%ED%95%9C%EA%B5%AD%ED%83%80%EC%9D%B4%EC%96%B4+when:1d&hl=ko&gl=KR&ceid=KR%3Ako')
 
-        self.slack_url = os.environ['SLACK_WEBHOOK']
+        # self.slack_url = os.environ['SLACK_WEBHOOK']
 
     def crawler(self):
         html = self.tstation
@@ -20,15 +20,27 @@ class tstation:
         answers = bs[0].findAll('h3', class_='ipQwMb ekueJc RD0gLb')
 
         for answer in answers:
-            data = {
-                "attachments": [{
-                    "color": "#36a64f",
-                    "title": answer.text,
-                    "title_link": f"https://www.news.google.com/{answer.find('a').get('href')}"
-                    }]
-                }
-            requests.post(url=self.slack_url, headers=self.headers, data=json.dumps(data))
+            print('title: ', answer.text)
+            print('URL: ', answer.find('a').get('href'))
+            # data = {
+            #     "attachments": [{
+            #         "color": "#36a64f",
+            #         "title": answer.text,
+            #         "title_link": f"https://www.news.google.com/{answer.find('a').get('href')}"
+            #         }]
+            #     }
+            # requests.post(url=self.slack_url, headers=self.headers, data=json.dumps(data))
 
 
-t = tstation()
-t.crawler()
+if __name__ == '__main__':
+    # sched = BackgroundScheduler(timezone="Asia/Seoul")
+    # sched.start()
+
+    # while True:
+        # 매일 11시에 한국타이어 뉴스 크롤링
+        # @sched.scheduled_job('cron', hour='11', id='am')
+        # def crawler_tstation_news():
+    crawl = tstation()
+    crawl.crawler()
+
+        # time.sleep(1)
